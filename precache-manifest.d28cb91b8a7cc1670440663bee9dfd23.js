0f63e3956bf37d440632f590dd7afd5ab5de007d
Edgar Rojas <edgarjrojasl@gmail.com>
2020-04-27 10:59:57 -0400

    Updates

---
+++ precache-manifest.d28cb91b8a7cc1670440663bee9dfd23.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fc01b5d8bd53ef71e506ed5083691d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d87b1b681951cb964d",
+    "url": "/static/css/2.580795d1.chunk.css"
+  },
+  {
+    "revision": "c30f8ad56bebd6403f0e",
+    "url": "/static/css/main.d08a080c.chunk.css"
+  },
+  {
+    "revision": "70d87b1b681951cb964d",
+    "url": "/static/js/2.d64f132f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d64f132f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30f8ad56bebd6403f0e",
+    "url": "/static/js/main.61edbd62.chunk.js"
+  },
+  {
+    "revision": "f46c2db62d41d8e54c0f",
+    "url": "/static/js/runtime-main.ba2a0364.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
